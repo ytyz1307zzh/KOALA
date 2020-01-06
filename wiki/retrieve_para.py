eb148e59021a4671600cfd4758778e1bd4358165
Zhihan Zhang <zhangzhihan@pku.edu.cn>
2020-01-06 17:47:23 +0800

    retrieve relevant sentences from wiki database using tf-idf similarity

--- wiki/retrieve_para.py
+++ wiki/retrieve_para.py
@@ -1,8 +1,16 @@
+'''
+ @Date  : 01/03/2020
+ @Author: Zhihan Zhang
+ @mail  : zhangzhihan@pku.edu.cn
+ @homepage: ytyz1307zzh.github.io
+'''
+
 import time
 import os
 import json
 import argparse
 import logging
+from typing import Dict, List
 import regex
 from tqdm import tqdm
 import sqlite3
@@ -16,6 +24,9 @@ from drqa import tokenizers
 from drqa.tokenizers import SimpleTokenizer
 from drqa.retriever import DocDB
 from drqa.retriever import utils
+
+import spacy
+spacy_nlp = spacy.load("en_core_web_sm", disable = ['tagger', 'ner'])
 
 DEFAULT_CONVERT_CONFIG = {
     'tokenizer': SimpleTokenizer,
@@ -42,51 +53,56 @@ def init(tokenizer_class, tokenizer_opts, db_class, db_opts, candidates=None):
     PROCESS_DB = db_class(**db_opts)
     Finalize(PROCESS_DB, PROCESS_DB.close, exitpriority=100)
     PROCESS_CANDS = candidates
+
+
 def fetch_text(doc_id):
     global PROCESS_DB
     return PROCESS_DB.get_doc_text(doc_id)
 
-def has_answer(answer, para_text, match, tokenizer):
-    """Check if a document contains an answer string.
 
-    If `match` is string, token matching is done between the text and answer.
-    If `match` is regex, we search the whole text with the regex.
+def has_answer(answer: str, para_text: str, tokenizer):
+    """
+    Check if a document contains an answer string.
     """
     text = tokenizer.tokenize(para_text).words(uncased=True)
-    if match == 'string':
-        # Answer is a list of possible string
-        for single_answer in answer:
-            single_answer = utils.normalize(single_answer)
-            single_answer = tokenizer.tokenize(single_answer)
-            single_answer = single_answer.words(uncased=True)
-            for i in range(0, len(text) - len(single_answer) + 1):
-                if single_answer == text[i: i + len(single_answer)]:
-                    return True
-    else:
-        raise ValueError('wrong match type!')
+    answer_list = answer.split(';')
+
+    for single_answer in answer_list:
+
+        single_answer = single_answer.strip().lower()
+        single_answer = utils.normalize(single_answer)
+        single_answer = tokenizer.tokenize(single_answer)
+        single_answer = single_answer.words(uncased=True)
+
+        for i in range(0, len(text) - len(single_answer) + 1):
+            if single_answer == text[i: i + len(single_answer)]:
+                return True
+
     return False
 
-def _split_doc(doc):
-    """Given a doc, split it into chunks (by paragraph)."""
-    curr = []
-    curr_len = 0
-    for split in regex.split(r'\n+', doc):
-        split = split.strip()
-        if len(split) == 0:
-            continue
-        # Maybe group paragraphs together until we hit a length limit
-        if len(curr) > 0 and curr_len + len(split) > 0:
-            yield ' '.join(curr)
-            curr = []
-            curr_len = 0
-        curr.append(split)
-        curr_len += len(split)
-    if len(curr) > 0:
-        yield ' '.join(curr)
+
+def split_doc(docs: List[str]) -> (List[str], List[List[int]]):
+    """
+    Split and flatten documents. Maintain a mapping from doc (index in
+    flat list) to split (index in flat list).
+    didx2sidx: List[doc_idx -> [split_idx_start, split_idx_end]]
+    """
+    flat_splits = []
+    didx2sidx = []
+    nlp_docs = list(spacy_nlp.pipe(docs))
+
+    for nlp in nlp_docs:
+        splits = [sent.strip() for sent in nlp.sents]
+        didx2sidx.append([len(flat_splits), -1])
+        flat_splits.extend(splits)
+        didx2sidx[-1][1] = len(flat_splits)
+
+    return flat_splits, didx2sidx
+
 
 class ConvertData2ParagraphClsInput(object):
     def __init__(self, tokenizer='', ranker_config=None, db_config=None, n_doc=5,
-                 num_workers=1, convert_bs=48, ngram=2, distant=False, small=False):
+                 num_workers=1, convert_bs=48, ngram=2, small=False, save_all=False):
 
         self.convert_bs = convert_bs
         self.small = small
@@ -120,29 +136,18 @@ class ConvertData2ParagraphClsInput(object):
             initargs=(self.tok_class, self.tok_opts, self.db_class, self.db_opts)
         )
 
-        self.distant = distant
 
-    def read_data_txt(self, data_file):
-        with open(data_file, 'r', encoding='utf-8') as fin:
-            return [json.loads(line.strip('\n')) for line in fin.readlines()]
-
-    def read_input(self, data_file):
-        assert os.path.isfile(data_file) is not False
-        data_examples = self.read_data_txt(data_file=data_file)
-        data_examples = self.filter_query(data_examples)
+    def read_input(self, data_file: str) -> List[Dict]:
+        """
+        Read the queries from data_file. The file should contain a list of Dicts,
+        which at least should have two fields: entity and paragraph.
+        """
+        assert os.path.isfile(data_file)
+        data_examples = json.load(open(data_file, 'r', encoding='utf8'))
         if self.small:
-            data_examples = data_examples[:200]
+            data_examples = data_examples[:5]
         return data_examples
 
-    def filter_query(self, data_examples):
-        new_data_examples = []
-        for example in tqdm(data_examples, desc='filter queries', total=len(data_examples)):
-            query_tokens = self.tokenizer.tokenize(example['question'])
-            query_words = query_tokens.ngrams(n=self.ngram, filter_fn=utils.filter_ngram)
-            if len(query_words) != 0:
-                new_data_examples.append(example)
-        logger.info('Process {} queries to {}'.format(len(data_examples), len(new_data_examples)))
-        return new_data_examples[:]
 
     def process(self, query, n_docs=5,
                 return_context=False):
@@ -152,15 +157,14 @@ class ConvertData2ParagraphClsInput(object):
         )
         return para_examples[0]
 
-    def process_batch(self, examples, n_docs=5, mode='train'):
+
+    def process_batch(self, examples, n_docs=5):
         """Run a batch of queries' paragraphs (more efficient)."""
         t0 = time.time()
-        queries = [example['question'] for example in examples]
-        gold_paras = [example['squad_context'] for example in examples]
-        answers = [example['answer'] for example in examples]
-        squad_ids = [example['squad_qid'] for example in examples]
-        squad_contexts = [example['squad_context'] for example in examples]
-        squad_answers = [example['squad_answers'] for example in examples]
+        queries = [example['paragraph'] for example in examples]
+        entities = [example['entity'] for example in examples]
+        para_ids = [example['id'] for example in examples]
+        topics = [example['topic'] for example in examples]
 
         logger.info('Processing %d queries...' % len(queries))
         logger.info('Retrieving top %d docs...' % n_docs)
@@ -183,79 +187,56 @@ class ConvertData2ParagraphClsInput(object):
         # Split and flatten documents. Maintain a mapping from doc (index in
         # flat list) to split (index in flat list).
         # didx2sidx: List[doc_idx -> [split_idx_start, split_idx_end]]
-        flat_splits = []
-        didx2sidx = []
-        for text in doc_texts:
-            splits = _split_doc(text)
-            didx2sidx.append([len(flat_splits), -1])
-            for split in splits:
-                flat_splits.append(split)
-            didx2sidx[-1][1] = len(flat_splits)
-        if len(flat_splits) != doc_texts:
+        flat_splits, didx2sidx = split_doc(doc_texts)
+        if len(flat_splits) != len(doc_texts):
             logger.info('{} doc texts are splited into {} splits'.format(len(doc_texts), len(flat_splits)))
+
         para_examples = []
         distant_positive_num = 0
+
         for qidx in range(len(queries)):
-            para_example = []
+            matched_para = []
             distant_num = 0
+
             for rel_didx, did in enumerate(all_docids[qidx]):
                 start, end = didx2sidx[did2didx[did]]
+
                 for sidx in range(start, end):
-                    if (len(queries[qidx]) > 0 and
-                        len(flat_splits[sidx].split()) > 2) and not has_answer(answer=answers[qidx],
+
+                    if self.save_all and (len(queries[qidx]) > 0 and
+                        len(flat_splits[sidx].split()) > 2) and not has_answer(answer=entities[qidx],
                                                                                para_text=flat_splits[sidx],
-                                                                               match='string',
                                                                                tokenizer=self.tokenizer):
-                        para_example.append({
+                        matched_para.append({
 
                             'id': (qidx, did, rel_didx, sidx),
                             'question': queries[qidx],
                             'document': flat_splits[sidx],
                             'label': '0',
-                            'squad_id': squad_ids[qidx],
-                            'squad_answers': squad_answers[qidx],
-                            'squad_context': squad_contexts[qidx],
                             'tfidf_score': all_doc_scores[qidx][rel_didx],
                         })
+
                     elif (len(queries[qidx]) > 0 and
-                        len(flat_splits[sidx].split()) > 2) and  has_answer(answer=answers[qidx],
-                                                                               para_text=flat_splits[sidx],
-                                                                               match='string',
-                                                                               tokenizer=self.tokenizer) and self.distant:
-                        para_example.append({
+                        len(flat_splits[sidx].split()) > 2) and has_answer(answer=entities[qidx],
+                                                                            para_text=flat_splits[sidx],
+                                                                            tokenizer=self.tokenizer):
+                        matched_para.append({
 
                             'id': (qidx, did, rel_didx, sidx),
                             'question': queries[qidx],
                             'document': flat_splits[sidx],
                             'label': '1',
-                            'squad_id': squad_ids[qidx],
-                            'squad_answers': squad_answers[qidx],
-                            'squad_context': squad_contexts[qidx],
                             'tfidf_score': all_doc_scores[qidx][rel_didx],
                         })
                         distant_num += 1
 
-            if not self.distant:
-                para_example.append({
 
-                        'id': (qidx, -1, -1),
-                        'question': queries[qidx],
-                        'document': gold_paras[qidx],
-                        'label': '1',
-                        'squad_id': squad_ids[qidx],
-                        'squad_answers': squad_answers[qidx],
-                        'squad_context': squad_contexts[qidx],
-                        'tfidf_score': 1000000.0,
-                    })
-            if self.distant and distant_num == 0:
+            if distant_num == 0:
                 logger.info('Warning, question {} does not have distant answers in top {}.'.format(
                     queries[qidx], self.n_doc))
-                if mode == 'train':
-                    logger.info('skiped this query.')
-                    continue
-            if distant_num != 0:
+            else:
                 distant_positive_num += 1
-            para_examples.append(para_example)
+            para_examples.append(matched_para)
 
         logger.info('Reading %d questions...' % len(para_examples))
 
@@ -263,7 +244,6 @@ class ConvertData2ParagraphClsInput(object):
                     (len(queries), time.time() - t0))
 
         return para_examples, distant_positive_num
-
 
     def write_data_paras(self, para_examples, data_file):
         logger.info('write paras of data file to {}'.format(data_file + '.para'))
@@ -273,26 +253,27 @@ class ConvertData2ParagraphClsInput(object):
                     fout.write(json.dumps(para_example, ensure_ascii=False))
                     fout.write('\n')
 
-    def convert(self, data_file, mode='train'):
+    def convert(self, data_file):
         para_examples = []
         data_num = 0
         data_examples  = self.read_input(data_file=data_file)
         data_examples_batchs = [data_examples[data_id * self.convert_bs: (data_id + 1) * self.convert_bs]
                          for data_id in range(len(data_examples) // self.convert_bs + 1)]
         positive_total = 0
+
         for examples_per_batch in tqdm(data_examples_batchs, desc='convert batch examples',
                                        total=len(data_examples_batchs)):
             data_num += len(examples_per_batch)
-            para_example_batch, positive_num = self.process_batch(examples_per_batch, n_docs=self.n_doc, mode=mode)
+            para_example_batch, positive_num = self.process_batch(examples_per_batch, n_docs=self.n_doc)
             positive_total += positive_num
             para_examples.extend(para_example_batch)
+
         assert len(data_examples) == data_num
 
-        if self.distant:
-            logger.info('Process {} examples to {} distant examples, in mode: {}'.format(len(data_examples),
-                                                                                         len(para_examples),
-                                                                                         mode))
-            logger.info('Recall in top {} is {}'.format(self.n_doc, positive_total * 1.0 / data_num))
+        logger.info('Process {} examples to {} distant examples'.format(len(data_examples),
+                                                                        len(para_examples),
+                                                                        ))
+        logger.info('Recall in top {} is {}'.format(self.n_doc, positive_total * 1.0 / data_num))
         self.write_data_paras(para_examples, data_file=data_file)
 
 
@@ -363,37 +344,32 @@ class ConvertWikiPage2Paragraph(object):
         conn.close()
 
 
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_file', help='data file to convert to paras for classification.', required=True)
-    parser.add_argument('--ret_path', help='retriver model path')
-    parser.add_argument('--db_path', help='data base path')
-    parser.add_argument('--num_workers', default=None)
-    parser.add_argument('--n_para', default=50, type=int, help='para number of retrieval')
-    parser.add_argument('--distant', default=1, type=int)
-    parser.add_argument('--small', default=0, type=int)
+    parser.add_argument('-data_file', help='data file to convert to paras for classification.', required=True)
+    parser.add_argument('-ret_path', help='retriver model path')
+    parser.add_argument('-db_path', help='data base path')
+    parser.add_argument('-batch', type=int, default=128, help='batch size in retrieval')
+    parser.add_argument('-num_workers', default=None, help='number of processes')
+    parser.add_argument('-n_doc', default=50, type=int, help='number of desired retrieval')
+    parser.add_argument('-small', default=False, action='store_true', help='if true, use a tiny part of data for debugging')
+    parser.add_argument('-save_all', default=False, action='store_true', help='if true, save all sentences no matter whether it matches'
+                                                                              'with the context or not')
     # parser.add_argument('--db_para_save_path', help='save path of paragraphs for db')
-    args = parser.parse_args()
-    if 'dev' in args.data_file or 'test' in args.data_file:
-        mode = 'dev'
-    elif 'train' in args.data_file:
-        mode = 'train'
-    else:
-        raise ValueError('data file did not contain dev or train')
-    if 'para' in args.ret_path:
-        assert 'para' in args.db_path
+    opt = parser.parse_args()
+
+    if 'para' in opt.ret_path:
+        assert 'para' in opt.db_path
     convert2para = ConvertData2ParagraphClsInput(
-                                                 num_workers=int(args.num_workers) if args.num_workers else None,
-                                                 convert_bs=128,
-                                                 ranker_config={'ret_opt': {'tfidf_path': args.ret_path}},
-                                                 db_config={'db_opt': {'db_path': args.db_path}},
-                                                 n_doc=args.n_para,
-                                                 small=bool(args.small),
-                                                 distant=bool(args.distant),
+                                                 num_workers=int(opt.num_workers) if opt.num_workers else None,
+                                                 convert_bs=opt.batch,
+                                                 ranker_config={'ret_opt': {'tfidf_path': opt.ret_path}},
+                                                 db_config={'db_opt': {'db_path': opt.db_path}},
+                                                 n_doc=opt.n_doc,
+                                                 small=opt.small,
+                                                 save_all=opt.save_all
                                                  )
-    convert2para.convert(args.data_file, mode)
+    convert2para.convert(opt.data_file)
 
     # cwpp = ConvertWikiPage2Paragraph(db_path='/home/t-pinie/facebook-drqa/data/wikipedia/docs.db', num_workers=cpu_count())
     # cwpp.convert_page_to_paragraph(save_path=args.db_para_save_path)
