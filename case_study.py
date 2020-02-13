f7f39792352513344f8eb2b175446585ac2ff72c
Zhihan Zhang <zhangzhihan@pku.edu.cn>
2020-02-13 21:41:52 +0800

    able to output attention weights in case study

--- case_study.py
+++ case_study.py
@@ -13,6 +13,7 @@ from Dataset import *
 from Model import *
 import os
 import re
+import pdb
 import argparse
 print(f'[INFO] Import modules time: {time.time() - import_start_time}s')
 torch.set_printoptions(threshold=np.inf)
@@ -25,8 +26,16 @@ parser.add_argument('-hidden_size', type=int, default=128, help="hidden size of 
 parser.add_argument('-lr', type=float, default=1e-3, help="learning rate")
 parser.add_argument('-dropout', type=float, default=0.5, help="dropout rate")
 parser.add_argument('-elmo_dropout', type=float, default=0.5, help="dropout rate of elmo embedding")
-parser.add_argument('-loc_loss', type=float, default=1.0, help="hyper-parameter to weight location loss and state_loss")
+parser.add_argument('-loc_loss', type=float, default=0.3, help="hyper-parameter to weight location loss and state_loss")
 parser.add_argument('-elmo_dir', type=str, default='elmo', help="directory that contains options and weight files for allennlp Elmo")
+
+parser.add_argument('-cpnet', type=str, default="ConceptNet/result/retrieval.json", help="path to conceptnet triples")
+parser.add_argument('-wiki', type=str, default="wiki/result/retrieval.json", help="path to wiki paragraphs")
+parser.add_argument('-cpnet_enc_class', type=str, default='roberta', help='conceptnet encoder class')
+parser.add_argument('-cpnet_enc_name', type=str, default='roberta-large', help='conceptnet encoder name')
+parser.add_argument('-cpnet_finetune', action='store_true', default=False, help='specify to fine-tune the language model')
+parser.add_argument('-cpnet_inject', choices=['state', 'location', 'both'], default='both',
+                    help='where to inject ConceptNet commonsense')
 
 parser.add_argument('-restore', type=str, default=None, help="restoring model path")
 parser.add_argument('-test_set', type=str, default="data/test.json", help="path to test set")
@@ -82,7 +91,8 @@ def predict_consistent_loc(pred_state_seq: List[str], pred_loc_seq: List[str]) -
     return consist_loc_seq
 
 
-def get_output(metadata: Dict, pred_state_seq: List[int], pred_loc_seq: List[int], gold_state_seq: List[int]) -> Dict:
+def get_output(metadata: Dict, pred_state_seq: List[int], pred_loc_seq: List[int],
+               gold_state_seq: List[int], cpnet_triples: List[str]) -> Dict:
     """
     Get the predicted output from generated sequences by the model.
     """
@@ -107,18 +117,21 @@ def get_output(metadata: Dict, pred_state_seq: List[int], pred_loc_seq: List[int
     result = {'id': para_id,
               'entity': entity_name,
               'total_sents': total_sents,
-              'prediction': prediction
+              'prediction': prediction,
+              'cpnet': cpnet_triples
               }
     return result
 
 
-def write_output(output: List[Dict], output_filepath: str, sentences: List[List[str]]):
+def write_output(output: List[Dict], output_filepath: str, sentences: List[List[str]],
+                 state_attn_log: List, loc_attn_log: List, cpnet_cands: int):
     """
     Reads the headers of prediction file from dummy_filepath and fill in the blanks with prediction.
     Prediction will be stored according to output_filepath.
     """
     output_file = open(output_filepath, 'w', encoding='utf-8')
     columns = ['para_id', 'timestep', 'entity', 'state', 'gold_state', 'location', 'gold_location', 'sentence']
+    columns += [f'cpnet_{idx}' for idx in range(1, cpnet_cands + 1)]
     output_file.write('\t'.join(columns) + '\n\n')
     assert len(sentences) == len(output)
     total_instances = len(output)
@@ -129,12 +142,17 @@ def write_output(output: List[Dict], output_filepath: str, sentences: List[List[
 
     for i in range(total_instances):
         instance = output[i]
+        cpnet_triples = instance['cpnet']
+        cpnet_line = ['' for _ in range(8)] + cpnet_triples
+        output_file.write('\t'.join(cpnet_line) + '\n')
         sentence_list = sentences[i]
         sentence_list.insert(0, 'N/A')
 
         para_id = instance['id']
         entity_name = instance['entity']
         total_sents = instance['total_sents']
+        state_attn_list = state_attn_log[i]
+        loc_attn_list = loc_attn_log[i]
 
         correct_state, correct_loc = 0, 0
 
@@ -142,6 +160,9 @@ def write_output(output: List[Dict], output_filepath: str, sentences: List[List[
             pred_state, gold_state, pred_loc, gold_loc = instance['prediction'][step_i]
 
             fields = [str(para_id), str(step_i), entity_name, pred_state, gold_state, pred_loc, gold_loc, sentence_list[step_i]]
+            if step_i > 0:
+                fields += [f'{state_attn:.2f}/{loc_attn:.2f}' for state_attn, loc_attn in
+                           zip(state_attn_list[step_i-1], loc_attn_list[step_i-1])]
 
             if pred_state == gold_state and step_i > 0:
                 correct_state += 1
@@ -179,6 +200,7 @@ def test(test_set, model):
     start_time = time.time()
     report_state_correct, report_state_pred = 0, 0
     report_loc_correct, report_loc_pred = 0, 0
+    cpnet_cands = 0
     output_result = []
     all_sentences = []
 
@@ -188,16 +210,20 @@ def test(test_set, model):
             paragraphs = batch['paragraph']
             char_paragraph = batch_to_ids(paragraphs)
             all_sentences.extend(batch['sentences'])
+            sentence_mask = batch['sentence_mask']
             entity_mask = batch['entity_mask']
             verb_mask = batch['verb_mask']
             loc_mask = batch['loc_mask']
             gold_loc_seq = batch['gold_loc_seq']
             gold_state_seq = batch['gold_state_seq']
+            cpnet_triples = batch['cpnet']
             metadata = batch['metadata']
+            cpnet_cands = len(cpnet_triples[0])
             num_cands = torch.IntTensor([meta['total_loc_cands'] for meta in metadata])
 
             if not opt.no_cuda:
                 char_paragraph = char_paragraph.cuda()
+                sentence_mask = sentence_mask.cuda()
                 entity_mask = entity_mask.cuda()
                 verb_mask = verb_mask.cuda()
                 loc_mask = loc_mask.cuda()
@@ -207,15 +233,18 @@ def test(test_set, model):
 
             test_result = model(char_paragraph=char_paragraph, entity_mask=entity_mask, verb_mask=verb_mask,
                                 loc_mask=loc_mask, gold_loc_seq=gold_loc_seq, gold_state_seq=gold_state_seq,
-                                num_cands=num_cands)
+                                num_cands=num_cands, sentence_mask=sentence_mask, cpnet_triples=cpnet_triples)
 
             pred_state_seq, pred_loc_seq, test_state_correct, test_state_pred,\
                 test_loc_correct, test_loc_pred = test_result
 
             batch_size = len(paragraphs)
             for i in range(batch_size):
-                pred_instance = get_output(metadata = metadata[i], pred_state_seq = pred_state_seq[i], pred_loc_seq = pred_loc_seq[i],
-                                           gold_state_seq = gold_state_seq[i].tolist())
+                pred_instance = get_output(metadata = metadata[i],
+                                           pred_state_seq = pred_state_seq[i],
+                                           pred_loc_seq = pred_loc_seq[i],
+                                           gold_state_seq = gold_state_seq[i].tolist(),
+                                           cpnet_triples = cpnet_triples[i])
                 output_result.append(pred_instance)
 
             report_state_correct += test_state_correct
@@ -232,12 +261,16 @@ def test(test_set, model):
            f'State Prediction Accuracy: {state_accuracy * 100:.3f}%, '
            f'Location Accuracy: {loc_accuracy * 100:.3f}%')
 
-    write_output(output = output_result, output_filepath = opt.output, sentences = all_sentences)
+    state_attn_log = model.StateTracker.CpnetMemory.AttnUpdate.attn_log
+    loc_attn_log = model.StateTracker.CpnetMemory.AttnUpdate.attn_log
+
+    write_output(output = output_result, output_filepath = opt.output, sentences = all_sentences,
+                 state_attn_log = state_attn_log, loc_attn_log = loc_attn_log, cpnet_cands = cpnet_cands)
     print(f'[INFO] Test finished. Time elapse: {time.time() - start_time}s')
 
 
 if __name__ == "__main__":
-    test_set = ProparaDataset(opt.test_set, is_test=True)
+    test_set = ProparaDataset(opt.test_set, cpnet_path=opt.cpnet, is_test=True)
 
     print('[INFO] Start loading trained model...')
     restore_start_time = time.time()
